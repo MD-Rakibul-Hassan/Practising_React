92a53c93b081d8fb7f6632e99b39d087f3b4e8ba
Rakibul-Hassan-Shehab <mdrakibulhassan2425@gmail.com>
2024-01-05 07:17:15 +0600

    React Practis App

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Index from "./Components/Index";
+const App = () => {
+    return (
+        <div className="text-center bg-gray-600 flex gap-4 justify-center p-10 h-screen flex-col text-white">
+            
+            <Index />
+        </div>
+    )
+}
+export default App;
---
+++ src/Components/Index.jsx
@@ -0,0 +1,11 @@
+
+import Parent_data from "./Get_Data_Form_Child/Parent_Data";
+
+function Index () {
+    return (
+        <div className="">
+            <Parent_data />
+        </div>
+    )
+}
+export default Index;
